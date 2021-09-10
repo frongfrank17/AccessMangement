0213026079d93c25d48e7cf2daf9826f1b856b53
ICEPAJINGKO <icepajingko@gmail.com>
2021-09-10 13:48:25 +0700

    testpush2

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -96,7 +96,7 @@ function value(number) {
                 field_id: '',
                 field: '',
                 role: 'guest',
-                active: 0,
+                active: 1,
                 updated: 'yyyy-mm-dd',
                 created: 'yyyy-mm-dd'
             },
